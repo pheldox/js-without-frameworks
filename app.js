193058523abdc89dd5b253ea8124617f5ab940a0
pheldox <felixaryee4@gmail.com>
2020-03-06 22:41:34 +0000

    selector for single elements

--- app.js
+++ app.js
@@ -1,67 +1,38 @@
-let val;
+// document.getElementById()
 
-val = document;
-val = document.all;
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+// console.log(document.getElementById('task-title'));
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+// // Get things from the element
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
+// const taskTitle = document.getElementById('task-title');
 
+// // Change styling
+// taskTitle.style.background = '#333';
+// taskTitle.style.color = '#fff';
+// taskTitle.style.padding = '5px';
+// // taskTitle.style.display = 'none';
 
+// // Change content
+// taskTitle.textContent = 'Task List';
+// taskTitle.innerText = 'My Tasks';
+// taskTitle.innerHTML = '<span style="color:red">Task List</span>';
 
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
+// document.querySelector()
 
-let scripts = document.scripts;
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
 
-let scriptsArr = Array.from(scripts);
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
 
-scriptsArr.forEach(function(script) {
-  console.log(script.getAttribute('src'));
-});
-
-// console.log(val);
-
-// let val;
-//  val = document;
-//  val = document.all;
-//  val = document.doctype;
-//  val = document.URL;
-//  val = document.domain;
-
-//  val = document.forms;
-//  val = document.forms[0];
-
-//  val = document.links[0].className;
-//  val = document.links[0].classList;
-
-//  val = document.scripts;
-
-//  val = document.scripts[1].getAttribute('src');
-
-//  let scripts = document.scripts;
-//  let scriptsArr = Array.from(scripts);
-
-// scriptsArr.forEach(function(script) {
-//   console.log(script);
-// });
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'yellow';
+document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
 
 
-// console.log(val);
+
