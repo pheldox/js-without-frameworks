7f25d272dc41fa5aa0c7349d3e2eec103f31070d
pheldox <felixaryee4@gmail.com>
2020-03-07 16:47:27 +0000

    event listeners

--- app.js
+++ app.js
@@ -1,50 +1,37 @@
-// REPLACE ELEMENT
+// document.querySelector('.clear-tasks').addEventListener('click', function(e){
+//   console.log('Hello World');
 
-// Create Element
-const newHeading = document.createElement('h2');
-// Add id
-newHeading.id = 'task-title';
-// New text node
-newHeading.appendChild(document.createTextNode('Task List'));
+//   //e.preventDefault();
+// });
 
-// Get the old heading
-const oldHeading = document.getElementById('task-title');
-//Parent
-const cardAction = document.querySelector('.card-action');
+document.querySelector('.clear-tasks').addEventListener('click', onClick);
 
-// Replace
-cardAction.replaceChild(newHeading, oldHeading);
+function onClick(e){
+  //console.log('Clicked');
 
-// REMOVE ELEMENT
-const lis = document.querySelectorAll('li');
-const list = document.querySelector('ul');
+  let val;
 
-// Remove list item
-lis[0].remove();
+  val = e;
 
-// Remove child element
-list.removeChild(lis[3]);
+  // Event target element
+  val = e.target;
+  val = e.target.id;
+  val = e.target.className;
+  val = e.target.classList;
 
-// CLASSES & ATTR
-const firstLi = document.querySelector('li:first-child');
-const link = firstLi.children[0];
+  // Event type
+  val = e.type;
 
-let val;
+  // Timestamp
+  val = e.timeStamp;
 
-// Classes
-val = link.className;
-val = link.classList;
-val = link.classList[0];
-link.classList.add('test');
-link.classList.remove('test');
-val = link;
+  // Coords event relative to the window
+  val = e.clientY;
+  val = e.clientX;
 
-// Attributes
-val = link.getAttribute('href');
-val = link.setAttribute('href', 'http://google.com');
-link.setAttribute('title', 'Google');
-val = link.hasAttribute('title');
-link.removeAttribute('title');
-val = link;
-
-console.log(val);
+  // Coords event relative to the element
+  val = e.offsetY;
+  val = e.offsetX;
+  //e.target.innerText = 'Hello';
+  console.log(val);
+}
