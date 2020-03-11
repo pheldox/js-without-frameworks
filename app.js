695fdb0efe2336644fdb641ceff94852084ceb52
pheldox <fellixaryee4@gmail.com>
2020-03-11 00:36:28 +0000

    fetch external api data

--- app.js
+++ app.js
@@ -1,63 +1,13 @@
-document.getElementById('loan-form').addEventListener('submit', function (e) {
-  document.getElementById('results').style.display = 'none';
-
-  document.getElementById('loading').style.display = 'block';
-
-  setTimeout(calculateResults, 2000);
-  e.preventDefault();
-});
-
-function calculateResults() {
-  console.log('calculating...');
-
-  const amount = document.getElementById('amount');
-  const interest  = document.getElementById('interest');
-  const years  = document.getElementById('years');
-  const monthlyPayment = document.getElementById('monthly-payment');
-  const totalPayment = document.getElementById('total-payment');
-  const totalInterest = document.getElementById('total-interest');
-
-  const principal = parseFloat(amount.value);
-  const calculatedInterest= parseFloat(interest.value) / 100 /12;
-  const calculatedPayments = parseFloat(years.value) * 12;
-
-  const x = Math.pow(1+ calculatedInterest, calculatedPayments);
-  const monthly = (principal*x*calculatedInterest) /(x-1);
+async function getUsers() {
+  // await response for the fetch call
+  const response = await fetch
+  ('https://jsonplaceholder.typicode.com/users');
   
-  
-  if(isFinite(monthly)) {
-    monthlyPayment.value = monthly.toFixed(2);
-    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
-    
-    document.getElementById('results').style.display = 'block';
+  // only proceed once its resolved
+  const data = await response.json();
 
-    document.getElementById('loading').style.display = 'none';
+  //only proceed once second promise is resloved
+  return data;
 
-
-  } else {
-      showError('An error occured');
-      
-  }
-  
 }
-
-function showError(error) {
-
-  const errorDiv = document.createElement('div');
-
-  const card = document.querySelector('.card');
-  const heading = document.querySelector('.heading');
-
-  errorDiv.className = 'alert alert-danger';
-
-  errorDiv.appendChild(document.createTextNode(error));
-
-  card.insertBefore(errorDiv, heading);
-
-  setTimeout(clearError, 3000)
-}
-
-function clearError() {
-  document.querySelector('.alert').remove();
-}
+getUsers().then(users => console.log(users));
